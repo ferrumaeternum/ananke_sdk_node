4c14d4fb4f884fe1cb63fb71b7f1d5434794f67f
bathiya_94 <bathiya_94@localhost>
2017-12-13 10:40:52 +0530

    Completed the code for app1

--- app.js
+++ app.js
@@ -1,30 +1,39 @@
 /**
  * Created by dar on 12/4/17.
- */
+ **/
 
 const sdk = require('./sdk.js');
 
 var options = {
-    appId: 'appId',
-    groupId: 'groupId',
-    deviceId: 'deviceId',
-    username: 'username',
-    password: 'password'
+    appId: 'a1476238',
+    groupId: 'g2349178',
+    deviceId: 'd2775428',
+    username: 'bathi',
+    password: '1234'
 };
 
 var mySdk = sdk(options);
 
 if(mySdk != null){
-    mySdk.begin();
+
+    mySdk.begin(onConnect,onMessage);
+
+
 
 }
 
 function onConnect() {
 
+    setTimeout(function () {
+        mySdk.sendMessage('This is app 1 on connect message.');
+    },5000);
+
 }
 
 function onMessage(message) {
-    console.log(messsage);
+    console.log('app 2 loop back: '+message);
+    mySdk.sendMessage('This is app 1 on connect message');
+
 }
 
 
